0f9d53cb0be5eee9dc387f0e07ad5dfc77011c7b
Blake Shurtz <blakeobeans@gmail.com>
2020-05-30 15:48:43 -0700

    ignored files

--- Old Analysis/R script.R
+++
@@ -1,9 +0,0 @@
-x <- Weather2015$CDD
-hist(x, breaks = 7, col = "blue", 
-     main = "Distribution of Daily CDD", xlab = "Cooling Degree Days (CDD)", ylab = "Frequency",
-      xlim = c(0,20), ylim = c(0,40))
-
-y <- Weather2016$V2
-hist(y, breaks = 7, col = "red", 
-     main = "Distribution of Daily CDD", xlab = "Cooling Degree Days (CDD)", ylab = "Frequency",
-     xlim = c(0,20), ylim = c(0,40))
